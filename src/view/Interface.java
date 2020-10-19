67b992b6cf25e59e9bb1ec8cc9e2248e57c7b623
Gabriel Galdino <ggaldino95@gmail.com>
2020-10-19 11:21:57 -0300

    First class: Position

--- src/view/Interface.java
+++ src/view/Interface.java
@@ -1,9 +1,13 @@
 package view;
 
+import boardgame.Position;
+
 public class Interface {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		Position pos = new Position(3, 5);
+		
+		System.out.println(pos);
 
 	}
 
