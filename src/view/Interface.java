992e1e7174ac348275de3760fad62facf6ade123
Gabriel Galdino <ggaldino95@gmail.com>
2020-10-21 11:28:59 -0300

    Moving pieces

--- src/view/Interface.java
+++ src/view/Interface.java
@@ -1,14 +1,31 @@
 package view;
 
+import java.util.Scanner;
+
 import chess.ChessMatch;
+import chess.ChessPiece;
+import chess.ChessPosition;
 
 public class Interface {
 
 	public static void main(String[] args) {
-		
-		ChessMatch chessMatch = new ChessMatch();
-		UI.printBoard(chessMatch.getPieces());
 
+		Scanner sc = new Scanner(System.in);
+		ChessMatch chessMatch = new ChessMatch();
+		
+		while(true) {
+			UI.printBoard(chessMatch.getPieces());
+			
+			System.out.println();
+			System.out.print("Source: ");
+			ChessPosition source = UI.readChessPosition(sc);
+			
+			System.out.println();
+			System.out.print("Target: ");
+			ChessPosition target = UI.readChessPosition(sc);
+			
+			ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+		}
 	}
 
 }
